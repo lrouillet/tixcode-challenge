ed8c6c3b83e980ee6ae53e7d32aee3e3ad981c6c
Leandro <leandrorouillet@live.com>
2021-02-27 19:53:36 -0300

    [TCC-17] Primera version de signin sin autenticacion en re render

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -9,18 +9,22 @@ import User from './views/User';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
+import AuthState from './context/authState';
+
 function App() {
   return (
-    <Router>
-        <Navbar />
-        <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/" component={Signup} />
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/user/:username" component={User} />
-        </Switch>
-        <Footer/>
-    </Router>
+    <AuthState>
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/" component={Signup} />
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/user/:username" component={User} />
+            </Switch>
+            <Footer/>
+        </Router>
+    </AuthState>
   );
 }
 
--- cliente/src/views/Signup.jsx
+++ cliente/src/views/Signup.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useEffect, useState, useContext} from 'react'
 import styled from 'styled-components';
 import backgroundImage from '../images/signup-bkgd.png';
+import AuthContext from '../context/authContext';
 
 const Container = styled.div`
     height: 100%;
@@ -23,25 +24,131 @@ const FormContainer = styled.div`
     }
 `
 
-const Signup = () => {
+const Signup = (props) => {
+
+    const authContext = useContext(AuthContext);
+    const { message, authenticated,  userSignUp } = authContext;
+
+    useEffect(() => {
+        if (authenticated) {
+            return props.history.push('/home');
+        }
+    }, [authenticated, props.history]);
+
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        address: '',
+        confirmPass: ''
+    });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        userSignUp(formData);
+    }
+
+    const onChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value 
+        });
+    }
+
     return (
         <Container>
             <FormContainer>
                 <h1 className="h1 text-center">Únete a nosotros</h1>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                            <label htmlFor="inputEmail4">Usuario</label>
-                            <input type="text" className="form-control" id="inputEmail" placeholder="Usuario" required={true} />
+                            <label htmlFor="inputUserName">Usuario</label>
+                            <input
+                                type="text"
+                                name="username"
+                                className="form-control"
+                                id="inputUserName"
+                                placeholder="Usuario"
+                                required={true}
+                                onChange={onChange}
+                            />
                         </div>
                         <div className="form-group col-md-6">
-                            <label htmlFor="inputPassword4">Contraseña</label>
-                            <input type="password" className="form-control" id="inputPassword" placeholder="Contraseña" required={true} />
+                            <label htmlFor="inputEmail">Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="form-control"
+                                id="inputEmail"
+                                placeholder="Email"
+                                required={true}
+                                onChange={onChange}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputPassword">Contraseña</label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                id="inputPassword"
+                                placeholder="Contraseña"
+                                required={true}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputPasswordConfirm">Confirmar Contraseña</label>
+                            <input
+                                type="password"
+                                name="confirmPass"
+                                className="form-control"
+                                id="inputPasswordConfirm"
+                                placeholder="Contraseña"
+                                required={true}
+                                onChange={onChange}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputName">Nombre</label>
+                            <input
+                                type="text"
+                                name="name"
+                                className="form-control"
+                                id="inputName"
+                                placeholder="Nombre"
+                                required={true}
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputSurname">Apellido</label>
+                            <input
+                                type="text"
+                                name="surname"
+                                className="form-control"
+                                id="inputSurname"
+                                placeholder="Apellido"
+                                required={true}
+                                onChange={onChange}
+                            />
                         </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputAddress">Dirección</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="Av. Corrientes 600, Buenos Aires, Argentina" required={true} />
+                        <input
+                            type="text"
+                            name="address"
+                            className="form-control"
+                            id="inputAddress"
+                            placeholder="Av. Corrientes 600, Buenos Aires, Argentina"
+                            required={true}
+                            onChange={onChange}
+                        />
                     </div>
                     <button type="submit" className="btn btn-primary w-100">Regístrate</button>
                 </form>
